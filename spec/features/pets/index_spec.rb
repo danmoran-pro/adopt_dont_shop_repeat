59617e5a6f51bb3fae452ebf4140cf02fe288beb
Daniel <danmoran.pro@gmail.com>
2020-01-29 08:52:34 -0700

    Added missing information to pets attributes

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -6,13 +6,16 @@ RSpec.describe "As a visitor" do
     shelter_1 = Shelter.create!(name: "Mike's Shelter", address: "1331 17th Street", city: "Denver", state: "CO", zip: 80202)
     shelter_2 = Shelter.create!(name: "Meg's Shelter", address: "50 Main Street", city: "Hershey", state: "PA", zip: 17033)
     
-    sparky = Pet.create!(name: 'Sparky', image: 'https://adopt-dont-shop.s3-us-west-1.amazonaws.com/images/west_highland_white_terrier_24.jpg', approximate_age: 5, sex: 'male')
-    peppo = Pet.create!(name: 'Peppo', image: 'https://adopt-dont-shop.s3-us-west-1.amazonaws.com/images/mexican_hairless_105.jpg', approximate_age: 13, sex: 'female')
-    
+    sparky = shelter_1.pets.create!(name: 'Sparky', image: 'https://adopt-dont-shop.s3-us-west-1.amazonaws.com/images/west_highland_white_terrier_24.jpg', approximate_age: 5, sex: 'male')
+    peppo  = shelter_2.pets.create!(name: 'Peppo', image: 'https://adopt-dont-shop.s3-us-west-1.amazonaws.com/images/mexican_hairless_105.jpg', approximate_age: 13, sex: 'female')
+
     visit pets_path
     expect(current_path).to eq("/pets")
 
     expect(page).to have_content(sparky.name)
+    expect(page).to have_content(sparky.image)
+    expect(page).to have_content(sparky.approximate_age)
+    expect(page).to have_content(sparky.sex)
     expect(page).to have_content(peppo.name)
 
     end
