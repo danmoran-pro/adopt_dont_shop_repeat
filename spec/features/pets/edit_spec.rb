8f0398cdf4fb17d1164bbcddf44930fff370c01d
Daniel <danmoran.pro@gmail.com>
2020-01-31 12:51:10 -0700

    links added to update and delete within index for pets and shelters

--- spec/features/pets/edit_spec.rb
+++ spec/features/pets/edit_spec.rb
@@ -2,13 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'As a visitor' do 
     describe "'When I visit a pet's show page" do
-        it 'Then I see a link to update the pet "Update Pet" and redirected to an edit form' do 
-            shelter_1 = Shelter.create!(name: "Dumb Friends League", address: "123 Sample St", city: "Denver", state: "CO", zip: 80220)
-            sparky = shelter_1.pets.create!(name: 'Sparky', image: 'https://adopt-dont-shop.s3-us-west-1.amazonaws.com/images/west_highland_white_terrier_24.jpg', approximate_age: 5, sex: 'male', adoptable: true)
+        before(:each) do 
+        @shelter_1 = Shelter.create!(name: "Dumb Friends League", address: "123 Sample St", city: "Denver", state: "CO", zip: 80220)
+        @sparky = @shelter_1.pets.create!(name: 'Sparky', image: 'https://adopt-dont-shop.s3-us-west-1.amazonaws.com/images/west_highland_white_terrier_24.jpg', approximate_age: 5, sex: 'male', adoptable: true)
+        @peppo  = @shelter_1.pets.create!(name: 'Peppo', image: 'https://adopt-dont-shop.s3-us-west-1.amazonaws.com/images/mexican_hairless_105.jpg', approximate_age: 13, sex: 'female')
+        end 
 
-            visit pet_path(sparky)
+        it 'Then I see a link to update the pet "Update Pet" and redirected to an edit form' do 
+
+            visit pet_path(@sparky)
             click_link "Update Pet"
-            expect(current_path).to eq(edit_pet_path(sparky.id))
+            expect(current_path).to eq(edit_pet_path(@sparky.id))
 
             name = "twinkle" 
             image = "https://adopt-dont-shop.s3-us-west-1.amazonaws.com/images/west_highland_white_terrier_24.jpg"
@@ -22,15 +26,25 @@ RSpec.describe 'As a visitor' do
             fill_in 'pet[sex]',	with: sex
             click_on "Update Pet"
 
-            expect(current_path).to eq(pet_path(sparky.id))
+            expect(current_path).to eq(pet_path(@sparky.id))
             
-            expect(page).to have_css("img[src *= '#{sparky.image}']")
+            expect(page).to have_css("img[src *= '#{@sparky.image}']")
             expect(page).to have_content(name)
             expect(page).to have_content(approximate_age)
             expect(page).to have_content(sex)
 
-            expect(page).to_not have_content(sparky.name)  
-            expect(page).to_not have_content(sparky.approximate_age)  
+            expect(page).to_not have_content(@sparky.name)  
+            expect(page).to_not have_content(@sparky.approximate_age)  
+        end
+    
+        it "Then I see a link to update the pet 'Update Pet' under each pet's information  and redirected to an edit form" do 
+            
+            visit pets_path
+
+            within("#pet-#{@sparky.id}") do
+                click_link "Update Pet"
+            end  
+            expect(current_path).to eq(edit_pet_path(@sparky.id))
         end
     end
 end
